32ffee494d8688bdb898c0be00dad5ef7ee9b975
Júlia Beatriz <101215446+jujuliabeat@users.noreply.github.com>
2023-10-22 23:32:32 -0300

    Tentando

---
+++ App/Routes/route.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Routes;
+
+use Slim\App;
+use Slim\Views\PhpRenderer;
+
+use App\Controllers\PlayerController;
+use App\Controllers\TeamController;
+use App\Controllers\HomeController;
+use App\Controllers\InstallationController;
+
+$settings = [
+    'settings' => ['displayErrorDetails' => true],
+];
+
+$app = new App($settings);
+
+
+ // Get container
+ $container = $app->getContainer();
+
+ // Register component on container
+ $container['view'] = new PhpRenderer(__DIR__ . "/../Views/");
+ $container['BASE_PATH'] = 'http://localhost:8080';
+
+//Adicione suas rotas aqui!
+$app->get('/', HomeController::class . ":home");
+$app->get('/teams', TeamController::class . ":getAll");
+$app->get('/teams/{id}', TeamController::class . ":getById");
+$app->get('/teams/group/{group}', TeamController::class . ":getByGroup");
+$app->get('/teams/name/{name}', TeamController::class . ":getByName");
+
+$app->get('/players', PlayerController::class . ":getAll");
+$app->get('/players/{id}', PlayerController::class . ":getById");
+$app->get('/players/name/{name}', PlayerController::class . ":getByName");
+$app->get('/players/team/{id}', PlayerController::class . ":getByTeamId");
+$app->get('/players/position/{pos}', PlayerController::class . ":getByPosition"); 
+
+$app->get('/players/search/{search}', PlayerController::class . ":getBySearchParam");
+
+$app->run();
